dc72982fcda46c31c94e61db0ed19387cdff31c9
Siriannelr <siriannelery@gmail.com>
2025-12-10 11:43:11 +0100

    Meilleur CSS

--- concours.php
+++ concours.php
@@ -26,7 +26,7 @@ usort($data['photos'], function($a, $b) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Concours Photo - La Motte</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <header>
